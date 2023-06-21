a1c7bde4d84fbc355774495c57a0afed16760cf0
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-06-21 12:23:01 +0530

    Update views.py_added logging

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -65,6 +65,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.error('Invalid login attempt for username %s', form.username.data)
             flash("Invalid username or password")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
@@ -113,6 +114,7 @@ def logout():
         # Wipe out user and its token cache from session
         session.clear()
         # Also logout from your tenant's web session
+        app.logger.info(f"User {username} is logging out")
         return redirect(
             Config.AUTHORITY
             + "/oauth2/v2.0/logout"
